e4d1f75d42c0aedd411432c1a805ec2510558a4e
Emre Dikmen <101067129+fabyo0@users.noreply.github.com>
2024-11-10 17:27:31 +0300

    feat: add scope filter week/month

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -23,7 +23,7 @@ class Order extends Model
     protected function casts(): array
     {
         return [
-            'order_date' => 'date:m/d/Y',
+            'order_date' => 'date:d/m/Y',
         ];
     }
 
@@ -40,39 +40,53 @@ class Order extends Model
 
     public function scopeSearchByUserName($query, $name)
     {
-        return $query->when(! empty($term) | strlen($name), function () use ($query, $name) {
-            return $query->where('users.name', 'like', '%'.$name.'%');
+        return $query->when(!empty($term) | strlen($name) > 2, function () use ($query, $name) {
+            return $query->where('users.name', 'like', '%' . $name . '%');
         });
     }
 
     public function scopeFilterByDate($query, $min, $max)
     {
-        return $query->when(is_numeric($min), fn ($query) => $query->where('orders.order_date', '>=', $min * 100))
-            ->when(is_numeric($max), fn ($query) => $query->where('orders.order_date', '<=', $max * 100));
+        return $query->when(is_numeric($min), fn($query) => $query->where('orders.order_date', '>=', $min * 100))
+            ->when(is_numeric($max), fn($query) => $query->where('orders.order_date', '<=', $max * 100));
     }
 
     public function scopeFilterBySubTotal($query, $min, $max)
     {
-        return $query->when(is_numeric($min), fn ($query) => $query->where('orders.subtotal', '>=', $min * 100))
-            ->when(is_numeric($max), fn ($query) => $query->where('orders.subtotal', '<=', $max * 100));
+        return $query->when(is_numeric($min), fn($query) => $query->where('orders.subtotal', '>=', $min * 100))
+            ->when(is_numeric($max), fn($query) => $query->where('orders.subtotal', '<=', $max * 100));
     }
 
     public function scopeFilterByTotal($query, $min, $max)
     {
-        return $query->when(is_numeric($min), fn ($query) => $query->where('orders.total', '>=', $min * 100))
-            ->when(is_numeric($max), fn ($query) => $query->where('orders.total', '<=', $max * 100));
+        return $query->when(is_numeric($min), fn($query) => $query->where('orders.total', '>=', $min * 100))
+            ->when(is_numeric($max), fn($query) => $query->where('orders.total', '<=', $max * 100));
     }
 
     public function scopeFilterByTaxes($query, $min, $max)
     {
-        return $query->when(is_numeric($min), fn ($query) => $query->where('orders.taxes', '>=', $min * 100))
-            ->when(is_numeric($max), fn ($query) => $query->where('orders.taxes', '<=', $max * 100));
+        return $query->when(is_numeric($min), fn($query) => $query->where('orders.taxes', '>=', $min * 100))
+            ->when(is_numeric($max), fn($query) => $query->where('orders.taxes', '<=', $max * 100));
     }
 
     public function scopeByDays(Builder $query, int $days): Builder
     {
         return $query->select('order_date', \DB::raw("sum(total) as total"))
             ->where('order_date', '>=', now()->subDays($days))
+            ->groupBy('order_date');
+    }
+
+    public function scopeByWeek(Builder $query, int $weeks): Builder
+    {
+        return $query->select('order_date', \DB::raw("sum(total) as total"))
+            ->where('order_date', '>=', now()->addWeek($weeks))
+            ->groupBy('order_date');
+    }
+
+    public function scopeByMouth(Builder $query, int $month): Builder
+    {
+        return $query->select('order_date', \DB::raw("sum(total) as total"))
+            ->where('order_date', '>=', now()->addMonth($month))
             ->groupBy('order_date');
     }
 }
